340c323797a296cb2f2ea1092f0b82f24dabb171
okacchi-san <okacchi1979@gmail.com>
2021-03-19 17:06:14 +0900

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>自動販売機</h1>
+    
+    <div>
+      <button v-on:click="onSelect()">Add 1</button>
+      <p>Clicked {{ counter }} times.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    onSelect:function() {
+      this.counter++;
+    }
+  }
+};
+</script>
